8583ec3daf7a37086771d78be8bd22b25f1cd3ca
Robbendebiene <robbendebiene@mailbox.org>
2026-09-03 15:14:36 +0200

    Fix projection and enhance merging + filtering - Remove old obsolete code - Merge by name and additional distance - Pre-Filter overly large geometries

--- lib/main.sql
+++ lib/main.sql
@@ -1,6 +1,7 @@
 /**
  * Streams a remote GeoParquet file (preprocessed OpenStreetMap planet data) over HTTP,
- * filters for public transport elements, merges nearby elements and saves buffered areas locally.
+ * filters for public transport elements, merges nearby elements (and elements that share
+ * the same name) and saves buffered areas locally.
  * Writes the result to a FlatGeobuf file.
  **/
 
@@ -14,19 +15,24 @@ SET memory_limit = '4GB';
 -- Displays a progress bar in the terminal
 SET enable_progress_bar = true;
 SET enable_progress_bar_print = true;
-
+SET geometry_always_xy = true;
 -- Custom variables
 SET VARIABLE INPUT_FILE = "https://download.openplanetdata.com/osm/planet/geoparquet/v1/planet-latest.osm.parquet";
 SET VARIABLE OUTPUT_FILE = "output.fgb";
-SET VARIABLE DISTANCE_THRESHOLD = 100.0;
+-- Elements are merged if they are within this distance
+SET VARIABLE RAW_DISTANCE_THRESHOLD = 100.0;
+-- Elements that share the same (non-null) name are merged if they are within this distance
+SET VARIABLE NAME_DISTANCE_THRESHOLD = 300.0;
 SET VARIABLE BUFFER_DISTANCE = 50.0;
-
+SET VARIABLE MAX_AREA_LIMIT = 200000;
 -- Read, filter, and project data to a metric coordinate system
 
 CREATE OR REPLACE VIEW filtered_elements AS
 SELECT
-    ROW_NUMBER() OVER () AS id, -- Generate sequential IDs for cluster identification
-    ST_Transform(geometry, 'EPSG:3857') AS geometry,
+    -- Generate sequential IDs for cluster identification
+    ROW_NUMBER() OVER () AS id,
+    -- Project to mercator (meter) coordinate system to be able to use ST_DWithin or ST_Buffer later
+    ST_Transform(geometry::GEOMETRY('OGC:CRS84'), 'EPSG:3857') AS geometry,
     tags['name'] AS name
 FROM read_parquet(getvariable('INPUT_FILE'))
 WHERE
@@ -35,6 +41,11 @@ WHERE
     OR tags['public_transport'] = 'platform'
     OR tags['amenity'] = 'bus_station';
 
+-- Ignores overly large elements
+CREATE OR REPLACE VIEW filtered_by_size AS
+SELECT *
+FROM filtered_elements
+WHERE ST_Area(geometry) < getvariable('MAX_AREA_LIMIT');
 -- Run graph-based proximity clustering using ST_DWithin and stream out to FlatGeobuf
 -- If ST_ClusterWithin is ever supported by DuckDB, this could be simplified
 
@@ -43,9 +54,16 @@ COPY (
         SELECT DISTINCT
             a.id AS source_id,
             b.id AS target_id
-        FROM filtered_elements a
-        INNER JOIN filtered_elements b
-            ON ST_DWithin(a.geometry, b.geometry, getvariable('DISTANCE_THRESHOLD'))
+        FROM filtered_by_size a
+        INNER JOIN filtered_by_size b
+            -- link by max distance
+            ON ST_DWithin(a.geometry, b.geometry, getvariable('RAW_DISTANCE_THRESHOLD'))
+            -- link by name and separate max distance
+            OR (
+                a.name IS NOT NULL
+                AND a.name = b.name
+                AND ST_DWithin(a.geometry, b.geometry, getvariable('NAME_DISTANCE_THRESHOLD'))
+            )
     ),
 
     -- Recursively traverse chains to find the absolute minimum ID for each network cluster
@@ -83,13 +101,13 @@ COPY (
             MODE(g.name) AS name,
             ST_Buffer(ST_ConvexHull(ST_Collect(list(g.geometry))), getvariable('BUFFER_DISTANCE'), 2) AS geometry
         FROM final_clusters c
-        JOIN filtered_elements g
+        JOIN filtered_by_size g
         ON c.original_id = g.id
         GROUP BY c.global_cluster_id
     )
 
     SELECT
-        ST_Transform(geometry, 'EPSG:3857', 'EPSG:4326') AS geometry,
+        ST_Transform(geometry, 'OGC:CRS84') AS geometry,
         name
     FROM spatial_clusters
 )
